5a275f5c3e6bd0f40182462f75cbeca2c185a6d1
Rhari20 <hariharan20032000@gmail.com>
2021-05-27 23:19:42 +0530

    firebase config added

--- src/Login.js
+++ src/Login.js
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Login.css';
 import { Link } from 'react-router-dom';
 
 function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const signIn = (e) => {
+    e.preventDefault();
+  };
+
+  const register = (e) => {
+    e.preventDefault();
+  };
   return (
     <div className="login">
       <Link to="/">
@@ -18,16 +28,32 @@ function Login() {
 
         <form action="">
           <h5>E-mail</h5>
-          <input type="text" />
+          <input
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
 
           <h5>Password</h5>
-          <input type="password" />
+          <input
+            type="password"
+            value={password}
+            onchange={(e) => setPassword(e.target.value)}
+          />
 
-          <button className="login__signInButton">Sign In</button>
+          <button
+            type="submit"
+            onClick={signIn}
+            className="login__signInButton"
+          >
+            Sign In
+          </button>
         </form>
 
         <p>By Signing-in you agree to the Amazon Condition of use</p>
-        <button className="registerButton">Create your Amazon Account</button>
+        <button onClick={register} className="registerButton">
+          Create your Amazon Account
+        </button>
       </div>
     </div>
   );
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyA2rOrH6f_Q6IJeD1uCjdjtPY5IshtVbGY',
+  authDomain: 'clone-56fd8.firebaseapp.com',
+  projectId: 'clone-56fd8',
+  storageBucket: 'clone-56fd8.appspot.com',
+  messagingSenderId: '905898358624',
+  appId: '1:905898358624:web:dedc3c91f2096aa2421005',
+  measurementId: 'G-GR2V67V7HV',
+};
